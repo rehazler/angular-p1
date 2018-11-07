66c5d7c6e4615ed20606d3c98f8eb41511b708bb
rehazler <rehazler@gmail.com>
2018-11-06 20:58:26 -0800

    Day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-p1';
+  title: String = 'angular-p1';
+  name: String = 'Robert';
+  currentDay: any = new Date().getDay();
+  days: Object = {
+  	0: 'Sunday',
+  	1: 'Monday',
+  	2: 'Tuesday',
+  	3: 'Wednesday',
+  	4: 'Thursday',
+  	5: 'Friday',
+  	6: 'Saturday'
+  };
+
+  renderThisDay(){
+  	return this.days[this.currentDay];
+  }
 }
